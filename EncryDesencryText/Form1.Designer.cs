5774037661833f282aee6433ff005ebdfa735586
charlie <harlericho@gmail.com>
2020-12-14 11:07:32 -0500

    Add project files.

---
+++ EncryDesencryText/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace EncryDesencryText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.txtEncriptacion = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDesencriptar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Encriptacion:";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(184, 51);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(272, 20);
+            this.txtTexto.TabIndex = 2;
+            // 
+            // txtEncriptacion
+            // 
+            this.txtEncriptacion.Location = new System.Drawing.Point(184, 98);
+            this.txtEncriptacion.Name = "txtEncriptacion";
+            this.txtEncriptacion.Size = new System.Drawing.Size(272, 20);
+            this.txtEncriptacion.TabIndex = 3;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(344, 190);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(112, 23);
+            this.btnGenerar.TabIndex = 4;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(344, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Desencriptar:";
+            // 
+            // txtDesencriptar
+            // 
+            this.txtDesencriptar.Enabled = false;
+            this.txtDesencriptar.Location = new System.Drawing.Point(184, 148);
+            this.txtDesencriptar.Name = "txtDesencriptar";
+            this.txtDesencriptar.Size = new System.Drawing.Size(272, 20);
+            this.txtDesencriptar.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 254);
+            this.Controls.Add(this.txtDesencriptar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtEncriptacion);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "EncryptacionDesencrytacionTexto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.TextBox txtEncriptacion;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDesencriptar;
+    }
+}
+
---
+++ EncryDesencryText/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryDesencryText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //Encryptar
+        public byte[] Clave = Encoding.ASCII.GetBytes("Tu Password");
+        public byte[] IV = Encoding.ASCII.GetBytes("harlericho.86hAES");
+        public string EncriptaMD5(string Cadena)
+        {
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes(Cadena);
+            byte[] encripted;
+            RijndaelManaged cripto = new RijndaelManaged();
+            using (MemoryStream ms = new MemoryStream(inputBytes.Length))
+            {
+                using (CryptoStream objCryptoStream = new CryptoStream(ms, cripto.CreateEncryptor(Clave, IV), CryptoStreamMode.Write))
+                {
+                    objCryptoStream.Write(inputBytes, 0, inputBytes.Length);
+                    objCryptoStream.FlushFinalBlock();
+                    objCryptoStream.Close();
+                }
+                encripted = ms.ToArray();
+            }
+            return Convert.ToBase64String(encripted);
+        }
+
+        //Desencrytar
+        public string DesencriptaMD5(string Cadena)
+        {
+            byte[] inputBytes = Convert.FromBase64String(Cadena);
+            byte[] resultBytes = new byte[inputBytes.Length];
+            string textoLimpio = String.Empty;
+            RijndaelManaged cripto = new RijndaelManaged();
+            using (MemoryStream ms = new MemoryStream(inputBytes))
+            {
+                using (CryptoStream objCryptoStream = new CryptoStream(ms, cripto.CreateDecryptor(Clave, IV), CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(objCryptoStream, true))
+                    {
+                        textoLimpio = sr.ReadToEnd();
+                    }
+                }
+            }
+            return textoLimpio;
+        }
+
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            if (!txtTexto.Text.Equals(""))
+            {
+                txtEncriptacion.Text = EncriptaMD5(txtTexto.Text);
+                txtDesencriptar.Text = DesencriptaMD5(txtEncriptacion.Text);
+            }
+            else
+            {
+                string message = "Campo texto esta vacio";
+                string title = "Aviso";
+                MessageBox.Show(message, title);
+                txtTexto.Focus();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtEncriptacion.Text = "";
+            txtTexto.Text = "";
+            txtDesencriptar.Text = "";
+        }
+    }
+}
